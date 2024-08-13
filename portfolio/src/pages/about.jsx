8f764bf22f233d4a162b428f9d9618ce20177f9e
SANEH2015 <mkhizes834@gmail.com>
2024-08-13 16:01:06 +0200

    about

--- portfolio/src/pages/about.jsx
+++ portfolio/src/pages/about.jsx
@@ -1,9 +1,21 @@
+
 function Aboutpage(){
     
     
     return(
-        <>
-        <h1>About</h1>
+ <>
+  <div className="main">
+  <div className="picture">
+      
+      </div>
+    <div className="about">
+    <p >HELLO.</p>
+    <h1 style={{color:"blue"}} >I'm Sanele MKHIZE</h1>
+    <p>a fullstock developer</p>
+    </div>
+   
+    
+ </div>     
        
 </>
     )
